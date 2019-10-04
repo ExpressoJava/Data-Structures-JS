6f098afef9fda7f94294210c55d358c47e45df86
Bao Pham <ryanvuepham@gmail.com>
2019-10-04 15:57:41 -0500

    implements all hashtables functions

--- hash-tables.js
+++ hash-tables.js
@@ -2,18 +2,47 @@
 
 class HashTable {
 	constructor() {
-		this.storage = []
+		this._storage = []
+		this.tableSize = 25
 	}
 	/*
 	 * Insert a new key-value pair
 	 * @param {string} key - the key associated with the value
 	 * @param {*} value - the value to insert
 	 */
-	innsert() {}
-	/* 
-  * Delete a key-value pair
-  * @param {string} key 0 the key associated with the value
-  * @return {*} value - the deleted value
+	insert() {}
+	/*
+	 * Delete a key-value pair
+	 * @param {string} - the key associated with the value
+	 * @return {*} value - the deleted value
+	 */
+	remove() {}
+	/*
+	 * Returns the value associated with a key
+	 * @param {string} key - the key to search for
+	 * @return {*} - the value associated with the key
+	 */
+	retrieve() {}
+	/*
+	 * Hashes string value into an integer that can be mapped to an array index
+	 * @param {string} str - the string to be hashed
+	 * @param {number} n - the size of the storage array
+	 * @return {number} - an integer between 0 and n
+	 */
+	_hash(str, n) {
+		let sum = 0
+		for (let i = 0; i < str.length; i++) sum += str.charCodeAt(i) * 3
 
-  */
+		return sum % n
+	}
 }
+const myHT = new HashTable()
+
+console.log(myHT)
+
+myHT.insert('a', 1)
+myHT.insert('b', 1)
+
+// console.log(myHT)
+
+// HashTable { _storage: [0,0,0['a', 1],0,0,0] }
