7524add6c0e859ac4bb20f2a985d62a1144a8146
Bao Pham <ryanvuepham@gmail.com>
2019-10-04 15:40:24 -0500

    constructor and functions

---
+++ hash-tables.js
@@ -0,0 +1,19 @@
+/** Class repesenting a Hash Table **/
+
+class HashTable {
+	constructor() {
+		this.storage = []
+	}
+	/*
+	 * Insert a new key-value pair
+	 * @param {string} key - the key associated with the value
+	 * @param {*} value - the value to insert
+	 */
+	innsert() {}
+	/* 
+  * Delete a key-value pair
+  * @param {string} key 0 the key associated with the value
+  * @return {*} value - the deleted value
+
+  */
+}
