dfe974111cabb51a3f6d0e928ef4dae74aef8fa4
Bao Pham <ryanvuepham@gmail.com>
2019-10-04 17:21:56 -0500

    finish retrieve function

--- hashtables.js
+++ hashtables.js
@@ -1,5 +1,5 @@
 class HashTable {
-	constructor() {
+	constructor(val) {
 		this._storage = []
 		this._tableSize = val // initialize value
 	}
@@ -13,6 +13,7 @@ class HashTable {
 
 		if (!this._storage[index]) this._storage[index] = [] // if it doesnt extst, then we initialize it
 		// [0,0,0,[],]
+		// TODO: loop through array and find if key was already inserted
 		this._storage[index].push([key, value])
 		// [0,0,0,['a', 1, ['b', 2]]] // handling collision = hash func returns index for different key
 	}
@@ -27,7 +28,18 @@ class HashTable {
 	 * @param {string} key - the key to search for
 	 * @return {*} - the value associated with the key
 	 */
-	retrieve() {}
+	// { _storage: [0,0,0, ['a', 1], ['b', 2],0,0,0] }
+	retrieve(key) {
+		const index = this._hash(key, this._tableSize)
+		const arrayAtIndex = this._storage[index] // where arrayAtIndex = aray inside storage
+
+		if (arrayAtIndex) {
+			for (let i = 0; i < this._storage[index]; i++) {
+				const keyValueArray = arrayAtIndex[i] // key/value pair
+				if (keyValueArray[0] === key) return keyValueArray[1] // in this case we only know the key
+			}
+		}
+	}
 	/*
 	 * Hashes string value into an integer that can be mapped to an array index
 	 * @param {string} str - the string to be hashed
@@ -41,13 +53,13 @@ class HashTable {
 		return sum % n
 	}
 }
-const myHT = new HashTable()
+const myHT = new HashTable(25) // bucket value of 25. Can always double when is half-full
 
 console.log(myHT)
 
 myHT.insert('a', 1)
 myHT.insert('b', 2)
 
-// console.log(myHT)
+console.log(myHT)
 
 // HashTable { _storage: [0,0,0['a', 1],0,0,0] }
